a5d6141b4328dce738232a0baabe2da55f9083be
Eliazar Lopez Robles <eliazar.lopez@corvuscode.com>
2020-06-18 18:14:33 -0700

    Separetd InfoWindow from Map class

--- src/js/InfoWindow.js
+++ src/js/InfoWindow.js
@@ -1,8 +1,19 @@
 export default class InfoWindow {
-  static createInfoWindow() {
+  constructor() {
+    this.infoWindow = this.createInfoWindow();
+  }
+
+  createInfoWindow() {
     const infoWindow = new google.maps.InfoWindow({
       content: "Hola",
     });
     return infoWindow;
+  }
+
+  openInfo(marker, content, map) {
+    this.infoWindow.setContent(`
+    ${content}
+    `);
+    this.infoWindow.open(this.map, marker);
   }
 }
